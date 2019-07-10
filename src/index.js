e59b8258eaa022c55b21a7922d8f8458d4552c09
HappyJeannie <436030684@qq.com>
2019-07-11 00:29:45 +0800

    [add] - css extract plugin

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
+import '@assets/css/reset.css'
 document.write('hello, webpack!')
 document.write(...[1,2,3])
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,45 @@
 const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: path.resolve(__dirname, './src/index.js'),
+  output: {
+    filename: "[name].[hash].js",
+    path: path.resolve(__dirname, './dist')
+  },
+  module:{
+    rules:[
+      {
+        test:/\.css$/,
+        use:[
+          {
+            loader:MiniCssExtractPlugin.loader,
+            options:{
+              publicPath: '../'
+            }
+          }
+          ,'css-loader']
+      }
+    ]
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       tempFilePath: path.resolve(__dirname, './index.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
     })
   ],
-  output: {
-    filename: "[name].[hash].js",
-    path: path.resolve(__dirname, './dist')
+  resolve: {
+    alias:{
+      '@assets': path.resolve(__dirname,'./src/assets'),
+      '@utils': path.resolve(__dirname,'./src/utils'),
+      '@api': path.resolve(__dirname,'./src/api'),
+      '@src': path.resolve(__dirname,'./src')
+    }
   }
 }
