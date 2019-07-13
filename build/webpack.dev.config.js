33f2b506b092afb0ac4234f640e0efde29d85ad4
HappyJeannie <436030684@qq.com>
2019-07-13 16:46:24 +0800

    [修改] - 拆分webpack配置文件公共部分，使用 webpack-merge 合并配置文件

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,49 +1,14 @@
 const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const merge = require('webpack-merge')
+const baseWebpackConfig = require('./webpack.base.config')
 
-module.exports = {
-  entry: path.resolve(__dirname, '../src/pages/index.js'),
-  output: {
-    filename: "[name].[hash].js",
-    path: path.resolve(__dirname, '../dist')
-  },
+const devWebpackConfig = merge(baseWebpackConfig,{
   devServer: {
     contentBase: path.resolve(__dirname,'../dist'),
-    port: 9000
+    port: 8123,
+    open: true
   },
-  module:{
-    rules:[
-      {
-        test:/\.css$/,
-        use:[
-          {
-            loader:MiniCssExtractPlugin.loader,
-            options:{
-              publicPath: '../'
-            }
-          }
-          ,'css-loader']
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      tempFilePath: path.resolve(__dirname, '../index.html')
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css'
-    })
-  ],
-  resolve: {
-    alias:{
-      '@assets': path.resolve(__dirname,'../src/assets'),
-      '@utils': path.resolve(__dirname,'../src/utils'),
-      '@api': path.resolve(__dirname,'../src/api'),
-      '@src': path.resolve(__dirname,'../src')
-    }
-  }
-}
+  devtool: 'inline-source-map'
+})
+
+module.exports = devWebpackConfig
--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -1,45 +1,11 @@
 const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const merge = require('webpack-merge')
+const baseWebpackConfig = require('./webpack.base.config')
 
-module.exports = {
-  entry: path.resolve(__dirname, './src/index.js'),
+const prodWebpackConfig = merge(baseWebpackConfig,{
   output: {
-    filename: "[name].[hash].js",
-    path: path.resolve(__dirname, './dist')
-  },
-  module:{
-    rules:[
-      {
-        test:/\.css$/,
-        use:[
-          {
-            loader:MiniCssExtractPlugin.loader,
-            options:{
-              publicPath: '../'
-            }
-          }
-          ,'css-loader']
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      tempFilePath: path.resolve(__dirname, './index.html')
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css'
-    })
-  ],
-  resolve: {
-    alias:{
-      '@assets': path.resolve(__dirname,'./src/assets'),
-      '@utils': path.resolve(__dirname,'./src/utils'),
-      '@api': path.resolve(__dirname,'./src/api'),
-      '@src': path.resolve(__dirname,'./src')
-    }
+    publicPath: "./"
   }
-}
+})
+
+module.exports = prodWebpackConfig
